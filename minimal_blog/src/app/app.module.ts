2a1e39891f84d3c384fc13a293f0a00fa2cf8141
Uri Hanna <h165841@stud.u-szeged.hu>
2023-09-20 08:58:05 +0200

    komponensek létrehozása

---
+++ minimal_blog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+    ImageSliderComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
