aaf0eb3927fa1b8ea72b2362c2c911ee76ccba51
Uri Hanna <h165841@stud.u-szeged.hu>
2023-09-20 18:37:28 +0200

    card komponens szerkezet, stílus, szükséges képek

--- minimal_blog/src/app/card/card.component.ts
+++ minimal_blog/src/app/card/card.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  content: {
+    img: string;
+    date: string;
+    title: string;
+    desc: string;
+    author: string;
+    num: number;
+  }[] = [];
 
+  images: string[] = [
+    '../assets/card1.png',
+    '../assets/card2.png',
+    '../assets/card3.png',
+    '../assets/card4.png',
+    '../assets/card5.png',
+  ];
+
+  constructor() {
+    for (let i = 0; i < this.images.length; i++) {
+      this.content.push({
+        img: this.images[i],
+        date: '29.Jun.2021',
+        title: 'Richard Norton photorealistic rendering as real photos',
+        desc: 'Many years ago, I worked for my parents who own a video production company.',
+        author: 'By: Peter Rowardson',
+        num: 175,
+      });
+    }
+  }
 }
