83a3f7d1140c922568b627cf250d1f66ec28f3f8
Uri Hanna <h165841@stud.u-szeged.hu>
2023-09-24 11:37:32 +0200

    image slider megvalósíás

--- minimal_blog/src/app/image-slider/image-slider.component.ts
+++ minimal_blog/src/app/image-slider/image-slider.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
-  styleUrls: ['./image-slider.component.css']
+  styleUrls: ['./image-slider.component.css'],
 })
 export class ImageSliderComponent {
+  images: string[] = [
+    '../assets/blog.png',
+    '../assets/blog1.png',
+    '../assets/blog2.png',
+    '../assets/blog3.png',
+    '../assets/blog4.png',
+  ];
+  currentIndex: number = 0;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  prevSlide() {
+    if(this.currentIndex === 0) {
+      this.currentIndex = this.images.length;
+    }
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  nextSlide() {
+    if(this.currentIndex === this.images.length - 1) {
+      this.currentIndex = -1;
+    }
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    }
+  }
 }
