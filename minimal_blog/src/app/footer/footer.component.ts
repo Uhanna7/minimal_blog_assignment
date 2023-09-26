27e309ebb0fc937cf35fd8868c87363842953e3b
Uri Hanna <h165841@stud.u-szeged.hu>
2023-09-26 10:21:00 +0200

    footer reszponzívvá téve

--- minimal_blog/src/app/footer/footer.component.ts
+++ minimal_blog/src/app/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  isPhonePortrait = false;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe((result) => {
+      this.isPhonePortrait = false;
+
+      if (result.matches) {
+        this.isPhonePortrait = true;
+      }
+    });
+  }
 
 }
